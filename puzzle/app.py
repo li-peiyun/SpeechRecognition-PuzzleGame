0ef1ef7686e00aefd5960ecd7f9fbc32b7ac220a
huhukxg <kxg61@tongji.edu.cn>
2023-11-24 23:59:28 +0800

    完成语音识别api部分
    完成语音识别api部分

--- puzzle/app.py
+++ puzzle/app.py
@@ -1,5 +1,14 @@
 import ast
+
 from flask import Flask, render_template, request, jsonify
+from aip import AipSpeech
+import subprocess
+import tempfile
+
+APP_ID = '43554036'
+API_KEY = '6KGm6tAxGK6ooLkLODnxTEvH'
+SECRET_KEY = 'woFwxbwL3oa2fRzX5lIjk4fKqLhwje3p'
+ffmpeg_path = 'D:/AAAProgramFiles/ffmpeg-2023-11-22-git-0008e1c5d5-full_build/bin/ffmpeg'
 
 app = Flask(__name__)
 
@@ -28,6 +37,7 @@ def puzzle_page(detail):
     return render_template("puzzle.html", data=puzzle)
 
 
+
 # 音频处理
 @app.route('/process_audio', methods=['POST'])
 def process_audio():
@@ -37,16 +47,37 @@ def process_audio():
     puzzle_answer = request.form.get('puzzle_answer')
     # 接收音频文件
     audio_file = request.files.get('audio')
+    # 可以识别内容，形式错误
+    # audio_content = audio_file.read()
+
+    # 创建临时文件并保存上传的音频文件
+    with tempfile.NamedTemporaryFile(delete=False) as temp_audio_file:
+        temp_audio_path_original = temp_audio_file.name
+        audio_file.save(temp_audio_path_original)
+
+    # 使用 FFmpeg 进行音频格式转换
+    temp_audio_path_converted = tempfile.NamedTemporaryFile(delete=False).name
+    subprocess.run([ffmpeg_path, '-y', '-i', temp_audio_path_original, '-f', 's16le', '-ac', '1', '-ar', '16000', temp_audio_path_converted])
+
+
+    # 读取转换后的音频文件内容
+    with open(temp_audio_path_converted, 'rb') as converted_file:
+        audio_content = converted_file.read()
 
     #####################################
     # 在这里处理接收到的语音文件
     #####################################
+    client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
+    # 使用文件路径调用百度地图API
+    question_result = client.asr(audio_content, 'wav', 16000, {'dev_pid': 1537})
+    # 查看结果形式
+    # print(question_result)
     # 构建包含处理结果的字典对象
     result = {
         # 处理完成后需要替换以下内容
         # xwt替换question内容为检测的句子
-        'question': '男人死了吗？',
+        'question': question_result['result'][0],
         # lx替换answer内容为chatgpt回答
         'answer': '不是'
     }
